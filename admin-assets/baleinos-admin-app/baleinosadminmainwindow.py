77de02c755c4c60b41a8e9be7738e2ad6ce06eaf
Adrien Touzouli <contact@ad2ien.dev>
2024-08-14 17:10:24 +0200

    ♻️  make a class out of the Qwindow & save button

--- admin-assets/baleinos-admin-app/baleinosadminmainwindow.py
+++ admin-assets/baleinos-admin-app/baleinosadminmainwindow.py
@@ -8,6 +8,7 @@ from PySide6.QtUiTools import QUiLoader
 POLICIES_PATH="./"
 # POLICIES_PATH="/etc/firefox/policies/"
 POLICIES_FILE= POLICIES_PATH + "policies.json"
+POLICIES_INIT_FILE="./firefox-policies.json"
 POLICIES_BACKUP= POLICIES_PATH + "policies.json.bak"
 
 def is_sudo():
@@ -19,7 +20,9 @@ def load_firefox_policies():
             json_data = json.load(json_file)
             return json_data
     except:
-        return None
+        with open(POLICIES_INIT_FILE) as json_file:
+            json_data = json.load(json_file)
+            return json_data
 
 def get_homepage(json):
     try:
@@ -43,29 +46,51 @@ def disable_firefox_policies():
     os.rename(POLICIES_FILE, POLICIES_BACKUP)
     return True
 
-def save_firefox_policies(json):
-    # update json from form
-    with open(POLICIES_FILE, 'w') as outfile:
-        json.dump(json, outfile)
 
-def display_error(text):
+def display_error(text, e=None):
+    print(text,e)
     msgBox = QMessageBox()
     msgBox.setIcon(QMessageBox.Critical)
     msgBox.setText(text);
     msgBox.exec();
 
+
+class BaleinosAdmin(QApplication):
+    def __init__(self, argv):
+        super().__init__(argv)
+
+        self.json_policy = load_firefox_policies()
+
+        self.loader = QUiLoader()
+        self.window = self.loader.load("form.ui", None)
+        self.fill_form()
+
+    def fill_form(self):
+        self.window.sudo_indicator_label.setText("✅" if is_sudo() else "❌")
+        self.window.file_indicator_label.setText("✅" if os.path.isfile(POLICIES_FILE) else "❌")
+        self.window.remove_policy_button.clicked.connect(disable_firefox_policies)
+        self.window.save_button.clicked.connect(self.save_firefox_policies)
+        self.window.homepage_edit.setText(get_homepage(self.json_policy))
+        if self.json_policy is None:
+            self.window.message_content_label.setText("Firefox policies.json does not exist")
+
+    def save_firefox_policies(self):
+        # Check inputs
+
+        # update json from form
+        self.json_policy["policies"]["Homepage"]["URL"] = self.window.homepage_edit.text()
+
+        try:
+            with open(POLICIES_FILE, 'w') as outfile:
+                print(self.json_policy)
+                outfile.write(json.dumps(self.json_policy, indent=2))
+            self.window.message_content_label.setText("policies Firefox sauvegardées 👏👏")
+        except Exception as e:
+            display_error("Impossible de sauvegarder les modifications. Vérifier les droits d'écriture (sudo)",e)
+
+    def run(self):
+        self.window.show()
+        sys.exit(self.exec())
 if __name__ == "__main__":
-    json_policy = load_firefox_policies()
-        
-    app = QApplication(sys.argv)
-    loader = QUiLoader()
-    window = loader.load("form.ui",None)
-    window.sudo_indicator_label.setText( "✅" if is_sudo() else "❌" )
-    window.file_indicator_label.setText( "✅" if os.path.isfile(POLICIES_FILE) else "❌" )
-    window.remove_policy_button.clicked.connect(disable_firefox_policies)
-    window.homepage_edit.setText(get_homepage(json_policy))
-    if json_policy is None:
-        window.message_content_label.setText("Firefox policies.json does not exist")
-        # window.remove_policy_button.setEnabled(False)
-    window.show()
-    sys.exit(app.exec())
+    app = BaleinosAdmin(sys.argv)
+    app.run()
